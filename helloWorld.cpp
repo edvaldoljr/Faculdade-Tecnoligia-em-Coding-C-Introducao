88ccda864c7674ab184a4e266bc373d1a9f490ed
edvaldojr <edvaldoluizj@gmail.com>
2024-04-18 20:58:53 -0300

    Introdução a linguagem C++

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+/*Utilizamos '#include' para importa a biblioteca <Aqui dentro importamos a biblioteca que iremos usar em c++.
+biblioteca <iostream> que é uma biblioteca que disponibiliza funções de entrada e saída de dados.
+*/
+#include <iostream>
+
+/*Essa instrução permite utilizar as funções e métodos que estão presentes em std.*/
+using namespace std;
+
+/*main() Tudo que está dentro desse método, será executado. */
+int main(){
+    /*A função cout é uma função cujo objetivo é realizar impressões na tela do usuário*/
+    cout << "Hello World";
+    return 0;
+}
