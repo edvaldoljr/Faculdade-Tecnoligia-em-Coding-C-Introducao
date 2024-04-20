f96f684083e258b0b10aa2fd165bb627a4f70a27
edvaldojr <edvaldoluizj@gmail.com>
2024-04-20 01:26:12 -0300

    Laço de repetição while

---
+++ estruturaWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int i = 0;
+
+    while( i < 10 ) {
+        cout << "O valor de i é: " << i << endl;
+        i++;
+    }
+
+    return 0;
+}
