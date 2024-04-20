7dcbacdb2d4b18174f5d1e1a859659bbdbf8f084
edvaldojr <edvaldoluizj@gmail.com>
2024-04-20 01:30:34 -0300

    Laço de repetição do-while

---
+++ estruturaDoWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int i = 0;
+
+    do {
+        cout << "O valor de i é: " << i << endl;
+        i++;
+    } while(i < 10);
+
+    return 0;
+}
