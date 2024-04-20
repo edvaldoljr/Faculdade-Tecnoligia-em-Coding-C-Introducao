98f3adf04f43c5cfbce0fcd669bf8c3f7065aa70
edvaldojr <edvaldoluizj@gmail.com>
2024-04-19 23:45:21 -0300

    atualizando

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,10 +1,16 @@
-
+/*Utilizamos '#include' para importa a biblioteca <Aqui dentro importamos a biblioteca que iremos usar em c++.
+biblioteca <iostream> que é uma biblioteca que disponibiliza funções de entrada e saída de dados.
+*/
 #include <iostream>
 
+/*Essa instrução permite utilizar as funções e métodos que estão presentes em std.*/
 using namespace std;
 
+/*main() Tudo que está dentro desse método, será executado. */
 int main(){
+    /*A função cout é uma função cujo objetivo é realizar impressões na tela do usuário*/
     
+    /*A função endl em C++ é usada para inserir uma nova linha e também para "limpar" o buffer de saída. Ela é parte da biblioteca padrão iostream e é frequentemente usada junto com a saída (<<) em operações de fluxo, como com cout.*/
     cout << "Hello World" << endl;
     return 0;
 }
